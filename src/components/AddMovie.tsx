1aa68537bdb07273d09235aa9d8b62d3d47c9adf
CarinWood <carin.wood.85@gmail.com>
2024-02-20 11:26:48 +0100

    update

--- src/components/AddMovie.tsx
+++ src/components/AddMovie.tsx
@@ -3,30 +3,54 @@ import "./addMovie.css";
 
 const AddMovie = () => {
   const [inputVal, setInputVal] = useState("");
+  const [stars, setStars] = useState(1);
 
   const addRating = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     console.log(inputVal);
+    console.log(stars);
   };
 
   const clear = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log("clear");
     setInputVal("");
+    setStars(1)
+  };
+
+  const handleRangeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault;
+    setStars(parseInt(e.target.value));
   };
 
   return (
     <form>
       <div>
         <label className="title-label">Title:</label>
-        <input
-          type="text"
-          className="title-input"
-          value={inputVal}
-          onChange={(e) => setInputVal(e.target.value)}
-        />
+        <article>
+          <input
+            type="text"
+            className="title-input"
+            value={inputVal}
+            onChange={(e) => setInputVal(e.target.value)}
+          />
+        </article>
       </div>
-      <input type="range" />
+      <div className="rating-div">
+        <label className="range-label">Rating: </label>
+        <article>
+          <span>1</span>
+          <input
+            type="range"
+            min={1}
+            max={5}
+            step={1}
+            value={stars}
+            onChange={handleRangeChange}
+          />
+          <span>5</span>
+        </article>
+      </div>
+      <div>
       <select>
         <option value="Drama">Drama</option>
         <option value="Arama">Action</option>
@@ -38,6 +62,7 @@ const AddMovie = () => {
         <option value="Western">Western</option>
         <option value="Sci-fi">Sci-fi</option>
       </select>
+      </div>
       <textarea />
       <div>
         <button onClick={(e) => clear(e)}>Clear</button>
